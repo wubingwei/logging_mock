b063ed7a728cb828f19e4bb047d6d00f3cd84b48
bingwei.wu <wubingwei1994@126.com>
2022-06-28 11:46:36 +0800

    fluentd http

--- handler/frequency.go
+++ handler/frequency.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/wubingwei/logging_mock/mock"
 	"net/http"
-	"time"
 )
 
 func Ping(c *gin.Context) {
@@ -14,12 +16,19 @@ func Ping(c *gin.Context) {
 }
 
 func Frequency(c *gin.Context) {
-	fc := new(mock.Frequency)
-	if err := c.ShouldBindJSON(fc); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	sc := bufio.NewScanner(c.Request.Body)
+
+	for sc.Scan() {
+		fc := new(mock.Frequency)
+		if err := jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(sc.Bytes(), fc); err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println(*fc)
+		}
+
 	}
-	mock.ContainerObj.Count(fc.Region, time.Now().Unix()-fc.EventTime)
+
+	//mock.ContainerObj.Count(fc.Region, time.Now().Unix()-fc.EventTime)
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
 	})
--- mock/counter.go
+++ mock/counter.go
@@ -6,22 +6,21 @@ import (
 )
 
 type Frequency struct {
-	EventTime       int64   `json:"event_time"`        // 展示发生的时间戳
-	Adx             string  `json:"adx"`               // adx 名称,"mintegral、oppocn、doubleclick"...
-	Os              int32   `json:"os"`                // 操作系统，android/ios
-	CountryCode     string  `json:"country_code"`      // 国家标识
-	ReqPkgName      string  `json:"req_pkg_name"`      // 流量侧包名
-	AdType          int32   `json:"adtype"`            // M 体系下的 int 广告类型，dsp需要做映射。https://gitlab.mobvista.com/voyager/common/-/blob/master/enum/enum_adtype.go#L185
-	RequestId       string  `json:"request_id"`        // 请求id
-	OneId           string  `json:"one_id"`            // 设备唯一id
-	CampaignId      int64   `json:"campaign_id"`       // 单子id
-	CampaignPkgName string  `json:"campaign_pkg_name"` // 单子包名
-	UserActivation  uint8   `json:"user_activation"`   // 1: 拉活；2:拉新
-	BussinessType   uint8   `json:"bussiness_type"`    // 事件类型，1:impression;2:click;3:conversion;4:install
-	IsTryNew        bool    `json:"is_try_new"`        // dsp使用，ext3["try_new"] 为1则是试新单子，无或者为0则为其他单子
-	Price           float32 `json:"price"`             // dsp使用，实际成交价格,千次价格
-	OnlyIpua        bool    `json:"only_ipua"`         // 0:有合法设备id，1:只有ipua
-	Region          string  `json:"region"`            // 流量region
+	Region      string  `json:"rg"`  // 日志生产的region
+	EventTime   int64   `json:"t"`   // 展示发生的时间戳
+	Adx         string  `json:"adx"` // adx 名称,"mintegral、oppocn、doubleclick"...
+	Os          string  `json:"os"`  // 操作系统，android/ios
+	RequestType int32   `json:"rt"`  // m 流量请求类型,见下面枚举
+	CountryCode string  `json:"cc"`  // 国家标识
+	ReqPkgName  string  `json:"rpn"` // 流量侧包名
+	PublisherId int64   `json:"pid"` // 开发者id，adn日志中publsherId为"6028"的是dsp数据
+	AdType      string  `json:"at"`  // 广告类型
+	RequestId   string  `json:"rid"` // 请求id
+	OneId       string  `json:"oid"` // 设备唯一id
+	EventType   int32   `json:"et"`  // 展示、点击、安装/转化。1:impression;2:click;3:conversion;4:install
+	CampaignId  int64   `json:"cid"` // 单子id
+	TryNew      bool    `json:"tn"`  // dsp使用，ext3["try_new"] 为1则是试新单子，无或者为0则为其他单子
+	Price       float64 `json:"p"`   // dsp使用，实际成交价格,千次价格
 }
 
 var ContainerObj Container
